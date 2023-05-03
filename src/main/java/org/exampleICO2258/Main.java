39782af6ac1e91124044152d54f0bbf107f94352
Gabo <cristhiancarmona32@aragon.unam.mx>
2023-05-03 10:15:04 -0600

    tarea9

---
+++ src/main/java/org/exampleICO2258/Main.java
@@ -0,0 +1,9 @@
+package org.exampleICO2258;
+
+import org.exampleICO2258.IU.Principal;
+
+public class Main {
+    public static void main(String[] args) {
+        Principal ventana = new Principal("Hola mundo AWT");
+    }
+}
